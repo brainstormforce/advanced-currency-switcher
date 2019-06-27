e515fcbcf2bea53aef9b22b884b5db6628649ea2
Mahesh Waghmare <maheshw@bsf.io>
2019-06-27 15:20:30 +0530

    Improvements.

--- assets/js/cs-script.js
+++ assets/js/cs-script.js
@@ -2,6 +2,9 @@
 
     $(document).ready(function(){
         $( '.cs-convertor-wrap-symbol' ).html( csVars.base_currency_symbol );
+
+        cs_currency_calculations( csVars.base_currency );
+
         if( $( '.cs-currency-name' ).length ) {
             $( '.cs-currency-name' ).hide();
             $( '.cs-currency-name' ).eq(0).show();
@@ -9,8 +12,53 @@
         }
     });
 
-    $(document).on(
-        'click', '.cs-currency-name', function () {
+    function cs_currency_calculations( currency_name ) {
+
+        if( currency_name ) {
+
+            //console.log( csVars.actual_currency_rates[ currency_name ] );
+            if(currency_name==='INR') {
+                var symb=csVars.currency_symbol_add['inr-symbol'];
+
+            } else if(currency_name==='USD') {
+                var symb=csVars.currency_symbol_add['usd-symbol'];;
+
+
+            } else if(currency_name==='AUD') {
+                var symb=csVars.currency_symbol_add['aud-symbol'];;
+
+            } else if(currency_name==='EUR') {
+                var symb=csVars.currency_symbol_add['eur-symbol'];;
+            }
+            
+            var decimalpoint = csVars.decimal_point;
+            var myarray = [];
+            var rate_inr = csVars.actual_currency_rates[ currency_name ];
+
+            jQuery('.cs-convertor-wrap-data').each(
+                function () {
+                    var mydata = ( jQuery(this).attr("valuemy") );
+                    myarray.push(mydata);
+                    var arrayLength = myarray.length;
+                    for ( var i = 0; i < arrayLength; i++ ) {
+                        var converted_value = myarray[i] * rate_inr;
+                        converted_value = converted_value.toFixed(decimalpoint).replace(/\.?0+$/, '');
+                        var spans = document.querySelectorAll(".cs-convertor-wrap-data");// get all the elements with id=cs-converter-wrap-1
+                        for ( var j = i; j <= i; j++ ) {
+                            spans[j].innerHTML = converted_value; //set the innerHTML as hello
+                        }
+                        var symbol = document.querySelectorAll(".cs-convertor-wrap-symbol");
+                        for ( var k = i; k <= i; k++ ) {
+                            symbol[k].innerHTML = symb; //set the Currency Symbol
+                        }
+                    }
+                }
+            );
+
+        }
+    }
+
+    $(document).on( 'click', '.cs-currency-name', function () {
 
             $( '.cs-currency-name' ).hide();
             if( undefined == $(this).next().attr('data-currency-name') ) {
@@ -21,48 +69,7 @@
 
             var currency_name = $(this).attr('data-currency-name') || '';
 
-            if(currency_name ) {
-
-                //console.log( csVars.actual_currency_rates[ currency_name ] );
-                if(currency_name==='INR') {
-                    var symb=csVars.currency_symbol_add['inr-symbol'];
-
-                } else if(currency_name==='USD') {
-                    var symb=csVars.currency_symbol_add['usd-symbol'];;
-
-
-                } else if(currency_name==='AUD') {
-                    var symb=csVars.currency_symbol_add['aud-symbol'];;
-
-                } else if(currency_name==='EUR') {
-                    var symb=csVars.currency_symbol_add['eur-symbol'];;
-                }
-                
-                var decimalpoint = csVars.decimal_point;
-                var myarray = [];
-                var rate_inr = csVars.actual_currency_rates[ currency_name ];
-
-                jQuery('.cs-convertor-wrap-data').each(
-                    function () {
-                        var mydata = ( jQuery(this).attr("valuemy") );
-                        myarray.push(mydata);
-                        var arrayLength = myarray.length;
-                        for ( var i = 0; i < arrayLength; i++ ) {
-                            var converted_value = myarray[i] * rate_inr;
-                            converted_value = converted_value.toFixed(decimalpoint).replace(/\.?0+$/, '');
-                            var spans = document.querySelectorAll(".cs-convertor-wrap-data");// get all the elements with id=cs-converter-wrap-1
-                            for ( var j = i; j <= i; j++ ) {
-                                spans[j].textContent = converted_value; //set the textContent as hello
-                            }
-                            var symbol = document.querySelectorAll(".cs-convertor-wrap-symbol");
-                            for ( var k = i; k <= i; k++ ) {
-                                symbol[k].textContent = symb; //set the Currency Symbol
-                            }
-                        }
-                    }
-                );
-
-            }
+            cs_currency_calculations( currency_name );
     }
     );
 
@@ -70,53 +77,9 @@
     $('.cs-currency-name-dropdown').change(function () {
           //console.log($('.cs-currency-name-dropdown').val());
           //console.log(csVars.actual_currency_rates.INR);
-          
-          if ('EUR' == $('.cs-currency-name-dropdown').val()) {
-             var symb='€';
-             var rate_inr = csVars.actual_currency_rates.EUR
-            
-          }
-          if ('INR' == $('.cs-currency-name-dropdown').val()) {
-            var symb='₹';
-             var rate_inr = csVars.actual_currency_rates.INR
-            
-          }
-          if ('USD' == $('.cs-currency-name-dropdown').val()) {
-            var symb='$';
-             var rate_inr = csVars.actual_currency_rates.USD
-            
-          }
-          if ('AUD' == $('.cs-currency-name-dropdown').val()) {
-            var symb='$';
-            var rate_inr = csVars.actual_currency_rates.AUD
-           
-          }
-           var decimalpoint = csVars.decimal_point;
-           var myarray = [];
-           
-
-          jQuery('.cs-convertor-wrap-data').each(
-                    function () {
-                        var mydata = ( jQuery(this).attr("valuemy") );
-                        myarray.push(mydata);
-                        var arrayLength = myarray.length;
-                        for ( var i = 0; i < arrayLength; i++ ) {
-                            var converted_value = myarray[i] * rate_inr;
-                            converted_value = converted_value.toFixed(decimalpoint).replace(/\.?0+$/, '');
-                            var spans = document.querySelectorAll(".cs-convertor-wrap-data");// get all the elements with id=cs-converter-wrap-1
-                            for ( var j = i; j <= i; j++ ) {
-                                spans[j].textContent = converted_value; //set the textContent as hello
-                            }
-
-                            var symbol = document.querySelectorAll(".cs-convertor-wrap-symbol");
-                            for ( var k = i; k <= i; k++ ) {
-                                symbol[k].textContent = symb; //set the Currency Symbol
-                            }
-                        }
-                    }
-                );
-    }
-    );
+          var currency_name = $('.cs-currency-name-dropdown').val() || '';
+          cs_currency_calculations( currency_name );
+    });
 
 
 } )(jQuery);
