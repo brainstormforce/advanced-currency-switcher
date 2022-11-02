3a3a7d0eaf00927df19d78d55a39a941a026d5f3
Swapnil Deshpande <swapnilde@bsf.io>
2022-11-02 13:36:45 +0530

    updated for wp 6.1

--- classes/class-cs-loader.php
+++ classes/class-cs-loader.php
@@ -98,7 +98,7 @@ class CS_Loader {
 	 * @return void
 	 */
 	public function define_constant() {
-		define( 'CSWP_CURRENCY_SWITCHER_VER', '1.0.4' );
+		define( 'CSWP_CURRENCY_SWITCHER_VER', '1.0.5' );
 
 		define( 'CSWP_CURRENCY_SWITCH_FILE', trailingslashit( dirname( dirname( __FILE__ ) ) ) . 'currency-switcher.php' );
 
